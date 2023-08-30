476ff25a18992bfa8043ce2d3de97f98ca8166a9
SlyAimer <2289782085@qq.com>
2023-08-30 23:52:28 +0800

    project init

--- cilcil-common/src/test/java/com/cilcilcommon/CilcilCommonApplicationTests.java
+++ cilcil-common/src/test/java/com/cilcilcommon/CilcilCommonApplicationTests.java
@@ -3,7 +3,7 @@ package com.cilcilcommon;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class CilcilCommonApplicationTests {
 
     @Test
